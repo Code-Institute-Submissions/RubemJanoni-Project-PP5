ad30f8b4a9c2ac77508e7a499f30fcd7905e7d93
RubemJanoni <>
2023-11-21 22:27:56 +0000

    Created home,about,products and contact page

--- myecommerce/urls.py
+++ myecommerce/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home_page, about_page, contact_page, products_page
 
 urlpatterns = [
+    path('', home_page),
+    path('about/', about_page),
+    path('products/', products_page),
+    path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
---
+++ myecommerce/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_page(request):
+    return render(request, "home.html", {})
+
+
+def about_page(request):
+    return render(request, "about.html", {})
+
+
+def products_page(request):
+    return render(request, "products.html", {})
+
+
+def contact_page(request):    
+    return render(request, "contact.html", {})
