d2edf97eaff88f605b845e8cd64984295e3e582d
RubemJanoni <>
2023-11-24 00:41:59 +0000

    changed layout home page

--- myecommerce/urls.py
+++ myecommerce/urls.py
@@ -20,7 +20,6 @@ from .views import home_page, about_page, contact_page
 urlpatterns = [
     path('', home_page),
     path('about/', about_page),
-    path('products/', products_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
