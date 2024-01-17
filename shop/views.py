e51482d6991ebf30ee90fdfa1a1eb729b59a9e33
RubemJanoni <>
2024-01-17 21:45:01 +0000

    Customized admin

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class Make_login (LoginView):
+    template_name = 'login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('shop:List_publication')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Login successfully. Welcome!')
+        return response
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        messages.error(self.request, 'Something is wrong, Try again.')
+        return response
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(self.get_success_url())
+
+        return super().get(request, *args, **kwargs)
