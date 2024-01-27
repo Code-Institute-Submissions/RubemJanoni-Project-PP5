5bb1777d363667da7d34799527d3160dd9f86d33
Rubem Janoni <rubemjanoni@gmail.com>
2024-01-27 00:56:15 +0000

    created order class model

--- shop/models.py
+++ shop/models.py
@@ -96,16 +96,6 @@ class Payment(models.Model):
 
     
 class Order(models.Model):
-    # Usuario que fez o pedido
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # Itens do pedido
-    items = models.ManyToManyField(CartItem)
-    # Preço  total de pedido
-    total_price = models.DecimalField(max_digits=8, decimal_places=2)
-    # Hora e data em que o pedido foi feito
-    order_date = models.DateTimeField(auto_now_add=True)
+    
 
-    def __str__(self):
-        return f'Order - {self.user.username} - {self.order_date}'
 
-        
