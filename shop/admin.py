297c3a84a1b2301ad67ff57dbed18b0cfce6af9e
Rubem Janoni <rubemjanoni@gmail.com>
2024-01-27 01:01:04 +0000

    updated shop admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Cart, CartItem
+from .models import Product, Category, Cart, CartItem, Order
 
 # Register your models here.
 
@@ -28,3 +28,4 @@ admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Cart)
 admin.site.register(CartItem)
+admin.site.register(Order)
