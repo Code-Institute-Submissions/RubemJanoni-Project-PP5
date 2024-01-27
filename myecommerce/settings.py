05d4b438535227f65f73d350175dff0bbe711e6a
Rubem Janoni <rubemjanoni@gmail.com>
2024-01-27 14:24:52 +0000

    updated requirements.txt

--- myecommerce/settings.py
+++ myecommerce/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'allauth',
+    'django_countries',
     'allauth.account',
     'allauth.socialaccount',
     'cloudinary',
