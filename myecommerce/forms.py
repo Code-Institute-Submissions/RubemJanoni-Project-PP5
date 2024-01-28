cf380903335a0e87f781aba39c7595222853534f
Rubem Janoni <rubemjanoni@gmail.com>
2024-01-28 23:35:59 +0000

    Updated signup form

--- myecommerce/forms.py
+++ myecommerce/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from allauth.account.forms import SignupForm
+
 
 class ContactForm(forms.Form):
     email = forms.CharField(
