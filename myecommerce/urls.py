179c975e3f7dfb996b9ecac30441894e78ae96c8
Rubem Janoni <rubemjanoni@gmail.com>
2024-01-28 20:40:15 +0000

    Updated signup template

--- myecommerce/urls.py
+++ myecommerce/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_page, about_page, contact_page, register_page, login_page, logout_page
+from .views import home_page, about_page, contact_page
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
-    path('contact/', contact_page, name='contact'),
-    path('login/', login_page, name='login'),
-    path('register/', register_page, name='register'),
-    path('logout/', logout_page, name='logout'),
+    path('contact/', contact_page, name='contact'),     
     path('accounts/', include('allauth.urls')),
 
     # Abaixo, url do shop
