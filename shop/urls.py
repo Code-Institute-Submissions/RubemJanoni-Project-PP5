6b220a07aa953c69cdcad713b3e1f4ac72cf4940
Rubem Janoni <rubemjanoni@gmail.com>
2024-02-03 01:06:20 +0000

    updated shop urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,6 @@
 # store/urls.py
 from django.urls import path
-from .views import add_to_cart, CartView, ProductListView, OrderListView, Produto_Detail, OrderCreateView
+from .views import add_to_cart, CartView, ProductListView, OrderListView, Produto_Detail, OrderCreateView, CartItemDeleteView
 from myecommerce.views import login_in_detailview, AddressCreateView, AddressDeleteView, AddressUpdateView, UserProfileView
 
 
@@ -9,16 +9,16 @@ urlpatterns = [
     path('login/', login_in_detailview, name='login_view'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/', CartView.as_view(), name='cart'),
+    path('cart/item/<int:pk>/delete/', CartItemDeleteView.as_view(), name='cart-item-delete'),
     path('products/', ProductListView.as_view(), name='product_list'),
     path('order/create/', OrderCreateView.as_view(), name='order_create'),
     path('orders/', OrderListView.as_view(), name='order_list'),
     # Perfil User
-    path('user/<int:pk>/', UserProfileView.as_view(), name='user-profile'),
+    path('user/<int:pk>', UserProfileView.as_view(), name='user-profile'),
 
     # ADDRESS
-    path('address/create/', AddressCreateView.as_view(), name='address-create'),
+    path('address/create', AddressCreateView.as_view(), name='address-create'),
     path('address/<int:pk>/delete/', AddressDeleteView.as_view(), name='address-delete'),
     path('address/<int:pk>/update/', AddressUpdateView.as_view(), name='address-update'),
-
-    
+ 
 ]
