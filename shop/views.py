12d901cb613220dcefa6ccff6f135e0f3d1b763c
Rubem Janoni <rubemjanoni@gmail.com>
2024-02-03 01:05:15 +0000

    updated shop view

--- shop/views.py
+++ shop/views.py
@@ -8,9 +8,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
 from myecommerce.forms import LoginForm
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from django.urls import reverse_lazy
-from .models import Order, CartItem, Cart
+from .models import Order, CartItem, Cart, Payment, OrderItem
 
 
 class Produto_Detail(DetailView):
@@ -45,13 +45,14 @@ def add_to_cart(request, product_id):
         cart_item.save()
 
     # Atualiza o número de itens no carrinho na sessão do usuário
-    request.session['cart_items'] = CartItem.objects.filter(cart=cart).count()
+    request.session['cart_items'] = CartItem.objects.filter(cart=cart, is_purchased=False).count()
 
     # Converte o QuerySet para uma lista antes de calcular o valor total do carrinho
     cart_items = list(cart.cartitem_set.all())
     
     # Calcula o valor total do carrinho e converte para float antes de armazenar na sessão
     cart_total = round(sum(float(item.product.price) * item.quantity for item in cart_items),3)
+    
     request.session['cart_total'] = cart_total
     
     messages.success(request, 'Product added successfully.')
@@ -59,11 +60,10 @@ def add_to_cart(request, product_id):
     return redirect('home')
 
 
-
 class OrderCreateView(LoginRequiredMixin, CreateView):
     model = Order
     template_name = 'store/order_purchase.html'
-    fields = ['shipping_address', 'billing_address', 'payment']
+    fields = ['shipping_address', 'billing_address']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -78,8 +78,7 @@ class OrderCreateView(LoginRequiredMixin, CreateView):
         context['total_items'] = sum(cart_item.quantity for cart_item in user_cart_items)
 
         # Calcular o valor total da ordem
-        context['total_price'] = round(sum(float(cart_item.product.price) * cart_item.quantity for cart_item in user_cart_items),3)
-
+        context['total_price'] = round(sum(float(cart_item.product.price) * cart_item.quantity for cart_item in user_cart_items), 3)
 
         return context
 
@@ -101,31 +100,66 @@ class OrderCreateView(LoginRequiredMixin, CreateView):
         form.instance.total_price = total_price
         form.instance.ordered = False  # ou True, dependendo da sua lógica
 
-         # Adiciona uma mensagem de sucesso
+        # Chama o método form_valid da superclasse para salvar o objeto Order
+        response = super().form_valid(form)
+
+        # Cria automaticamente um Payment associado a este pedido
+        payment = Payment.objects.create(
+            user=user,
+            amount=total_price,
+            # stripe_charge_id='',  # so remova o comentário se tiver uma API stripe https://stripe.com/br
+        )
+
+        # Associa o Payment criado ao Order
+        self.object.payment = payment
+        self.object.save()
+
+        # Itera sobre os itens do carrinho e define o campo is_purchased como true
+        for cart_item in cart_items:
+            cart_item.is_purchased = True
+            cart_item.save()
+
+        # Limpa as sessões
+        if 'cart_items' in self.request.session:
+            del self.request.session['cart_items']
+
+        if 'cart_total' in self.request.session:
+            del self.request.session['cart_total']
+
+        # Limpa o carrinho após a criação do pedido
+        cart.products.clear()
+
+        # Adiciona uma mensagem de sucesso
         messages.success(self.request, 'Pedido criado com sucesso!')
 
-        # Chama o método form_valid da superclasse para salvar o objeto Order
-        return super().form_valid(form)
+        # O reverse_lazy('nome_da_url') redireciona para a página especificada após a criação do objeto.
+        return response
 
-    # O reverse_lazy('nome_da_url') redireciona para a página especificada após a criação do objeto.
     success_url = reverse_lazy('order_list')
-
-
-
 
 
 class CartView(View):
     template_name = 'store/cart.html'
 
-  
-
     def get(self, request, *args, **kwargs):
-        cart_items = CartItem.objects.filter(cart__user = request.user)
+        cart_items = CartItem.objects.filter(cart__user = request.user,  is_purchased=False)
         context = {'cart_items': cart_items}
 
         return render(request, self.template_name, context)
     
+    
+class CartItemDeleteView(LoginRequiredMixin, DeleteView):
+    model = CartItem
+    template_name = 'store/cart_item_confirm_delete.html'  # Crie este template conforme necessário
 
+    def get_success_url(self):
+        return reverse_lazy('cart')  # Substitua pelo nome real da sua URL para o carrinho
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, 'Item removido do carrinho com sucesso!')
+        return super().delete(request, *args, **kwargs)
+    
+    
 class ProductListView(View):
     template_name = 'store/product_list.html'
 
