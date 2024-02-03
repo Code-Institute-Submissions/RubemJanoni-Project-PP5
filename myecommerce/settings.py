4978e0e6601cc4d51ffd32b2d6014aa214bb36a3
Rubem Janoni <rubemjanoni@gmail.com>
2024-02-03 20:30:48 +0000

    fix: template settings path

--- myecommerce/settings.py
+++ myecommerce/settings.py
@@ -32,7 +32,7 @@ CLOUDINARY_STORAGE = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-rubemjanoni-project-pp5-fjeqtzknh7.us2.codeanyapp.com',
