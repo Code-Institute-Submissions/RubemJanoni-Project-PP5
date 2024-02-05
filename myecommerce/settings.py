4aecbb93c2b7f0e6a6ed1c7fd18b55840a637835
Rubem Janoni <rubemjanoni@gmail.com>
2024-02-05 11:44:58 +0000

    updated settings

--- myecommerce/settings.py
+++ myecommerce/settings.py
@@ -32,7 +32,7 @@ CLOUDINARY_STORAGE = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-rubemjanoni-project-pp5-fjeqtzknh7.us2.codeanyapp.com',
